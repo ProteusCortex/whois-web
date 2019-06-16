fc03ca168bacd89142de9bfd2947ed6756732775
John Kitsantas <proteus@devoid.gr>
2019-06-16 21:07:13 +0300

    refactor(app): replace HTML content
    Replace the initial HTML content with an example Bootstrap navbar as a
    starting point.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy();
 	});
 
-	it(`should have as title 'kow-whois'`, () => {
+	it(`should have as title '{ whois.kow }'`, () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
-		expect(app.title).toEqual('kow-whois');
-	});
-
-	it('should render title in a h1 tag', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector('h1').textContent).toContain('Welcome to kow-whois!');
+		expect(app.title).toEqual('{ whois.kow }');
 	});
 });
