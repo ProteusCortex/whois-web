1620a4ea1a03aa827c52ffa7d1d2ff66a787e13f
= <proteus@devoid.gr>
2019-10-30 22:19:54 +0200

    feat(app): start implementing the search in the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PiosService } from './api/pios.service';
+import { SnapshotsService } from './api/snapshots.service';
 
 @Component({
 	selector: 'kwi-root',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = '{ whois.kow }';
+
+	searchForm = new FormGroup({
+		domain: new FormControl('', Validators.required)
+	});
+
+	constructor(
+		private pios: PiosService,
+		private snapshots: SnapshotsService
+	) { }
+
+	piosen() {
+		this.pios.ask(this.searchForm.value.domain)
+			.subscribe(res => {
+				console.log(res);
+			});
+	}
+
+	click() {
+		this.snapshots.all()
+			.subscribe(res => console.log(res));
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ApiModule } from './api/api.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApiModule } from './api/api.module';
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		PageNotFoundComponent,
 	],
 	imports: [
+		CommonModule,
 		BrowserModule,
-		AppRoutingModule,
-		ApiModule
+		FormsModule,
+		ReactiveFormsModule,
+		ApiModule,
+		AppRoutingModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
