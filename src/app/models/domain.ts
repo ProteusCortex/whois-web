ec8c3e2b5b491cf35abcc7da423f976aa4f1baec
John Kitsantas <proteus@devoid.gr>
2020-03-12 00:59:37 +0200

    feat: create the basic model and interfaces

---
+++ src/app/models/domain.ts
@@ -0,0 +1,11 @@
+export class Domain {
+	public id: number;
+	public name: string;
+	public handle: string;
+	public protocolNumber: string;
+	public creationDate: Date;
+	public expirationDate: Date;
+	public lastupdate: Date;
+	public createdAt: Date;
+	public updatedAt: Date;
+}
---
+++ src/app/models/name-server.ts
@@ -0,0 +1,6 @@
+export class NameServer {
+	public id: number;
+	public name: string;
+	public createdAt: Date;
+	public updatedAt: Date;
+}
