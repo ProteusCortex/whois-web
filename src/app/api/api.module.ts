7e91a06de06ba3082eda15cb509ed7695ac99c8a
John Kitsantas <proteus@devoid.gr>
2020-03-29 01:38:00 +0200

    feat(api): create placeholder services for snapshots and domains

--- src/app/api/api.module.ts
+++ src/app/api/api.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
+import { DomainsService } from './domains.service';
 import { PiosService } from './pios.service';
+import { SnapshotsService } from './snapshots.service';
 
 @NgModule({
 	declarations: [],
 	imports: [],
 	providers: [
-		PiosService
+		PiosService,
+		SnapshotsService,
+		DomainsService
 	]
 })
 export class ApiModule { }
