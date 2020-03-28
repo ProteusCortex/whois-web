b2795e5c77e3af7dcf328bd4f55c07e0ea1a6ff6
John Kitsantas <proteus@devoid.gr>
2020-03-29 01:31:20 +0200

    feat(api): create the API module

---
+++ src/app/api/api.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { PiosService } from './pios.service';
+
+@NgModule({
+	declarations: [],
+	imports: [],
+	providers: [
+		PiosService
+	]
+})
+export class ApiModule { }
