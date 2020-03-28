98171b8a25686920ffbac7a39f4459292aba3bac
John Kitsantas <proteus@devoid.gr>
2020-03-29 01:34:34 +0200

    feat(model): add Snapshot to the models
    Add a class to describe a Snapshot. Add an index.ts file to make model
    imports tidier.

---
+++ src/app/models/snapshot.ts
@@ -0,0 +1,10 @@
+import { IPiosResult } from '../interfaces/pios-result.interface';
+import { Domain } from './domain';
+import { Registrar } from './registrar';
+import { NameServer } from './name-server';
+
+export class Snapshot implements IPiosResult {
+	public domain: Domain;
+	public registrar: Registrar;
+	public nameServers: NameServer[];
+}
