ec6856a2fe05ddbc2ec07d149caf9ebb6d53cdd7
John Kitsantas <proteus@devoid.gr>
2020-07-20 17:13:48 +0300

    refactor(models): add missing properties to Snapshot

--- src/app/models/snapshot.ts
+++ src/app/models/snapshot.ts
@@ -4,8 +4,11 @@ import { Registrar } from './registrar';
 import { NameServer } from './name-server';
 
 export class Snapshot implements IPiosResult {
+	public id: number;
 	public domain: Domain;
 	public registrar: Registrar;
 	public nameServers: NameServer[];
 	public isRegistered: boolean;
+	public createdAt: Date;
+	public updatedAt: Date;
 }
